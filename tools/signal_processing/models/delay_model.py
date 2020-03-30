9d11a2a144e6cde797bb0a77020d6e7cfa1fcbc3
marc-antoine <maheux.marcantoine@gmail.com>
2020-03-30 19:29:28 -0400

    Ajout du code pour le delay

--- tools/signal_processing/models/delay_model.py
+++ tools/signal_processing/models/delay_model.py
@@ -1,15 +1,28 @@
+import numpy as np
+
+
 class DelayModel:
     def __init__(self, configuration):
         self._configuration = configuration
+        self._delay = 0
+        self._volume = 0
 
     def delay_ranges(self):
-        return {'minValue': 0.1, 'maxValue': 2, 'resolution': 0.1}
+        return {'minValue': 0.0, 'maxValue': 2, 'resolution': 0.01}
 
     def volume_ranges(self):
-        return {'minValue': 0.1, 'maxValue': 2, 'resolution': 0.1}
+        return {'minValue': 0.0, 'maxValue': 0.9, 'resolution': 0.01}
 
     def update(self, delay, volume):
-        pass
+        self._delay = int(delay * self._configuration.fs)
+        self._volume = volume
 
     def process_audio(self, x):
-        return x
+        y = np.zeros(self._delay + x.shape[0])
+
+        for i in range(x.shape[0]):
+            y[i + self._delay] = x[i] + self._volume * y[i]
+
+        y = y[self._delay:]
+
+        return y
